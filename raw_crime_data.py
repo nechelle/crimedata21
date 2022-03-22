a775b16937a2435b89205216c4edf0319e57779f
Nechelle Burns <nburns1@gmail.com>
2022-03-22 18:35:57 -0400

    Add crime count, date generator

--- raw_crime_data.py
+++ raw_crime_data.py
@@ -1,23 +1,20 @@
 import pandas as pd
 import numpy as np
 
-#load data from online csv file, remove unneccessary columns, index by incident number
-crime_df = pd.read_csv('https://lky-open-data.s3.amazonaws.com/LMPD/CRIME_DATA_2021.csv', nrows=10)
+#load data from online csv file, remove unneccessary columns, index by incident number 
+crime_df = pd.read_csv('https://lky-open-data.s3.amazonaws.com/LMPD/CRIME_DATA_2021.csv')
 to_drop = ['DATE_OCCURED','UOR_DESC', 'NIBRS_CODE', 'UCR_HIERARCHY', 'ATT_COMP', 'LMPD_BEAT', 'BLOCK_ADDRESS']
 crime_df.drop(to_drop, inplace = True, axis = 1)
 crime_df.set_index('INCIDENT_NUMBER', inplace = True)
 
+#only populate sex crimes (make function:retrieved from input)
+a_crime = crime_df.loc[crime_df['CRIME_TYPE'] == 'SEX CRIMES']
 
-#only populate sex crimes
-a_crime = crime_df.loc[crime_df['CRIME_TYPE'] == 'ASSAULT']
-print(a_crime)
-#    += 1
 #print column headers
 print(a_crime.columns.tolist())
 
-#print zip code counts
-print(a_crime['ZIP_CODE'].value_counts())
-print(a_crime['ZIP_CODE'].describe())
+#print crime count
+print(len(a_crime))
 
 
 #list_type = ['VANDALISM', 'BURGLARY', 'ASSAULT', 'FRAUD', 'VEHICLE BREAK-IN/THEFT', 'OTHER', 'ROBBERY', 'THEFT/LARCENY', 'DRUGS/ALCOHOL VIOLATIONS', 'MOTOR VEHICLE THEFT', 'WEAPONS', 'HOMICIDE', 'ARSON', 'SEX CRIMES', 'DISTURBING THE PEACE']
--- sub_dates.py
+++ sub_dates.py
@@ -1,9 +1,54 @@
 import pandas as pd
-import numpy as np
+import random
+import datetime
+import matplotlib.pyplot as plt 
+import raw_crime_data
 
-def random_dates(start, end, n, unit='D', seed=None):
-    if not seed:
-      np.random.seed(0)
-  
-    ndays = (end - start).days + 1
-    return pd.to_timedelta(np.random.rand(n) * ndays, unit=unit) + start
+
+
+mytest = []
+#number to create random value from, use current time to get random number
+#random.seed(datetime.datetime.now())
+random.seed(27)
+
+def random_day():
+    ret_value = 0
+
+    random_value = random.randint(0, 65000)
+    if ((random_value % 5) != 0):
+        ret_value = random_value % 42
+    else:
+        if ((random_value % 42) == 0):
+            ret_value = random_value % 366
+        else:
+            ret_value = random_value % 42
+    return(ret_value)
+x_axis = []
+y_axis = []
+#print(raw_crime_data.a_crime)
+
+for i in range(len(raw_crime_data.a_crime)):
+    time_delta = random_day()
+    my_time_date = datetime.datetime.strptime(raw_crime_data.a_crime['DATE_REPORTED'][i],'%Y-%m-%d %H:%M:%S')
+    print(my_time_date)
+    timestamp = datetime.datetime.timestamp(my_time_date) + (86400 * time_delta)
+    print(timestamp)
+    x_axis.append(i + 1)
+    y_axis.append(time_delta)
+    mytest.append({'INCIDENT_NUMBER': raw_crime_data.a_crime.index[i],
+                   'DATE_REPORTED': raw_crime_data.a_crime['DATE_REPORTED'][i],
+                   'DATE_SUBMITTED': timestamp,
+                   'TIME_DELTA': time_delta})
+
+
+
+
+plt.figure(1)
+#plt.xticks(1)
+plt.scatter(x_axis, y_axis)
+#plt.plot([1,1])
+plt.title('Sex Crimes Submissions')
+plt.ylabel('Days until submission')
+plt.xlabel('Cases')
+plt.xticks(list(range(0, len(raw_crime_data.a_crime), 1)))
+plt.show()
