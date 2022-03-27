3a2c46c1be3f6f79d4633f95d0f8769b0637fb47
Nechelle Burns <nburns1@gmail.com>
2022-03-26 20:13:37 -0400

    changed visualization

--- sub_dates.py
+++ sub_dates.py
@@ -6,10 +6,15 @@ import raw_crime_data
 
 
 
+num_30_and_below = 0
+num_above_30 = 0
 mytest = []
+pie_chart_data = []
+
 #number to create random value from, use current time to get random number
-#random.seed(datetime.datetime.now())
-random.seed(27)
+random.seed(datetime.datetime.now().timestamp())
+#random.seed(27)
+
 
 def random_day():
     ret_value = 0
@@ -23,32 +28,38 @@ def random_day():
         else:
             ret_value = random_value % 42
     return(ret_value)
-x_axis = []
-y_axis = []
-#print(raw_crime_data.a_crime)
+
+def process_delta_for_graph(delta):
+    global num_above_30
+    global num_30_and_below
+
+    if delta > 30:
+        num_above_30 += 1
+    else:
+        num_30_and_below += 1
+
+def draw_graph():
+    pie_chart_data.append(num_30_and_below)
+    pie_chart_data.append(num_above_30)
+    pie_chart_labels = ['<= 30', '> 30']
+    plt.pie(pie_chart_data, labels = pie_chart_labels, autopct='%1.2f%%')
+    plt.title('Sex Crimes Submissions')
+    plt.show()
+
 
 for i in range(len(raw_crime_data.a_crime)):
     time_delta = random_day()
     my_time_date = datetime.datetime.strptime(raw_crime_data.a_crime['DATE_REPORTED'][i],'%Y-%m-%d %H:%M:%S')
-    print(my_time_date)
+    #print(my_time_date)
     timestamp = datetime.datetime.timestamp(my_time_date) + (86400 * time_delta)
-    print(timestamp)
-    x_axis.append(i + 1)
-    y_axis.append(time_delta)
+    #print(timestamp)
     mytest.append({'INCIDENT_NUMBER': raw_crime_data.a_crime.index[i],
                    'DATE_REPORTED': raw_crime_data.a_crime['DATE_REPORTED'][i],
                    'DATE_SUBMITTED': timestamp,
                    'TIME_DELTA': time_delta})
+    process_delta_for_graph(time_delta)
 
+print(">30:", num_above_30)
+print("<30:", num_30_and_below)
 
-
-
-plt.figure(1)
-#plt.xticks(1)
-plt.scatter(x_axis, y_axis)
-#plt.plot([1,1])
-plt.title('Sex Crimes Submissions')
-plt.ylabel('Days until submission')
-plt.xlabel('Cases')
-plt.xticks(list(range(0, len(raw_crime_data.a_crime), 1)))
-plt.show()
+draw_graph()  
