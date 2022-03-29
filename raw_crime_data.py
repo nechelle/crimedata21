20fc9f9e329bdca57bddb04b34b1e14e1639a10c
Nechelle Burns <nburns1@gmail.com>
2022-03-29 18:57:23 -0400

    added menu and user input req

--- raw_crime_data.py
+++ raw_crime_data.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import numpy as np
 
 #load data from online csv file, remove unneccessary columns, index by incident number 
 crime_df = pd.read_csv('https://lky-open-data.s3.amazonaws.com/LMPD/CRIME_DATA_2021.csv')
@@ -7,14 +6,60 @@ to_drop = ['DATE_OCCURED','UOR_DESC', 'NIBRS_CODE', 'UCR_HIERARCHY', 'ATT_COMP',
 crime_df.drop(to_drop, inplace = True, axis = 1)
 crime_df.set_index('INCIDENT_NUMBER', inplace = True)
 
-#only populate sex crimes (make function:retrieved from input)
-a_crime = crime_df.loc[crime_df['CRIME_TYPE'] == 'SEX CRIMES']
+a_crime = []
+crime_type = ""
+#create menu for user to choose crime, populates all crimes committed in 2021
+#asks for user input, outputs input
+def crime_menu():
+    crime_list = crime_df.CRIME_TYPE.unique()
+    for number, letter in enumerate(crime_list, start=1):
+        print(number, letter) 
+    
+    offense = input("Which criminal offense are you interested in? ")
+    print("You chose to look at the offense: " + offense)
 
-#print column headers
-print(a_crime.columns.tolist())
+    return offense
+#give number corresponding crimt type from specified list
+def set_crime_search(offense):
+  global a_crime
+  global crime_type
+  ret_val = 0
 
-#print crime count
-print(len(a_crime))
+  if offense == '1':
+    crime_type = 'VANDALISM'
+  elif offense == '2':
+    crime_type = 'BURGLARY'
+  elif offense == '3':
+    crime_type = 'ASSAULT'
+  elif offense == '4':
+    crime_type = 'VEHICLE BREAK-IN/THEFT'
+  elif offense == '5':
+    crime_type = 'OTHER'
+  elif offense == '6':
+    crime_type = 'ROBBERY'
+  elif offense == '7':
+    crime_type = 'THEFT/LARCENY'
+  elif offense == '8':
+    crime_type = 'MOTOR VEHICLE THEFT'
+  elif offense == '9':
+    crime_type = 'DRUGS/ALCOHOL VIOLATIONS'
+  elif offense == '10':
+    crime_type = 'WEAPONS'
+  elif offense == '11':
+    crime_type = 'ARSON'
+  elif offense == '12':
+    crime_type = 'HOMICIDE'
+  elif offense == '13':
+    crime_type = 'FRAUD'
+  elif offense == '14':
+    crime_type = 'SEX CRIMES'
+  elif offense == '15':
+    crime_type = 'DISTURBING THE PEACE'
+  else:
+    ret_val = -1
+    print("Offense not found.")
+#create variable for specified crime type retrieved from input
+  if ret_val == 0:
+    a_crime = crime_df.loc[crime_df['CRIME_TYPE'] == crime_type]
 
-
-#list_type = ['VANDALISM', 'BURGLARY', 'ASSAULT', 'FRAUD', 'VEHICLE BREAK-IN/THEFT', 'OTHER', 'ROBBERY', 'THEFT/LARCENY', 'DRUGS/ALCOHOL VIOLATIONS', 'MOTOR VEHICLE THEFT', 'WEAPONS', 'HOMICIDE', 'ARSON', 'SEX CRIMES', 'DISTURBING THE PEACE']
+  return ret_val
--- sub_dates.py
+++ sub_dates.py
@@ -8,27 +8,20 @@ import raw_crime_data
 
 num_30_and_below = 0
 num_above_30 = 0
-mytest = []
-pie_chart_data = []
+boundary_time = 50
 
 #number to create random value from, use current time to get random number
 random.seed(datetime.datetime.now().timestamp())
-#random.seed(27)
-
 
 def random_day():
-    ret_value = 0
-
-    random_value = random.randint(0, 65000)
-    if ((random_value % 5) != 0):
-        ret_value = random_value % 42
-    else:
-        if ((random_value % 42) == 0):
-            ret_value = random_value % 366
-        else:
-            ret_value = random_value % 42
+    global boundary_time
+    random.seed(datetime.datetime.now().timestamp())
+    ret_value = random.randint(0, boundary_time)
+    boundary_time = random.randint(28, 65)
+    
     return(ret_value)
 
+#generate totals from time data
 def process_delta_for_graph(delta):
     global num_above_30
     global num_30_and_below
@@ -38,28 +31,49 @@ def process_delta_for_graph(delta):
     else:
         num_30_and_below += 1
 
+#create pie chart percentages from time delta totals
 def draw_graph():
+    pie_chart_data = []
+    pie_chart_labels = []
     pie_chart_data.append(num_30_and_below)
     pie_chart_data.append(num_above_30)
-    pie_chart_labels = ['<= 30', '> 30']
+    #pie_chart_labels = ['<= 30', '> 30']
+    #pie_chart_labels = ('<= 30', '> 30')
+    pie_chart_labels.append('<=30 days')
+    pie_chart_labels.append('>30 days')
     plt.pie(pie_chart_data, labels = pie_chart_labels, autopct='%1.2f%%')
-    plt.title('Sex Crimes Submissions')
+    plt.title(raw_crime_data.crime_type + ' Submissions')
     plt.show()
 
+#create while loop for asking user input
+crime_option = 0
 
-for i in range(len(raw_crime_data.a_crime)):
-    time_delta = random_day()
-    my_time_date = datetime.datetime.strptime(raw_crime_data.a_crime['DATE_REPORTED'][i],'%Y-%m-%d %H:%M:%S')
-    #print(my_time_date)
-    timestamp = datetime.datetime.timestamp(my_time_date) + (86400 * time_delta)
-    #print(timestamp)
-    mytest.append({'INCIDENT_NUMBER': raw_crime_data.a_crime.index[i],
-                   'DATE_REPORTED': raw_crime_data.a_crime['DATE_REPORTED'][i],
-                   'DATE_SUBMITTED': timestamp,
-                   'TIME_DELTA': time_delta})
-    process_delta_for_graph(time_delta)
+while crime_option != 'Q':
+    crime_option = raw_crime_data.crime_menu()
+    result = raw_crime_data.set_crime_search(crime_option)
+    if crime_option.isalpha():
+        crime_option = crime_option.upper()
 
-print(">30:", num_above_30)
-print("<30:", num_30_and_below)
+    print('This offense occurred ', len(raw_crime_data.a_crime), 'times in 2021.')
 
-draw_graph()  
+#create TIME DELTA column and generate time_delta based on info in DATE REPORTED column
+    if result != -1:
+        mylist = []
+        for i in range(len(raw_crime_data.a_crime)):
+            time_delta = random_day()
+            my_time_date = datetime.datetime.strptime(raw_crime_data.a_crime['DATE_REPORTED'][i],'%Y-%m-%d %H:%M:%S')
+            #print(my_time_date)
+            timestamp = datetime.datetime.timestamp(my_time_date) + (86400 * time_delta)
+            #print(timestamp)
+            mylist.append({'INCIDENT_NUMBER': raw_crime_data.a_crime.index[i],
+                        'DATE_REPORTED': raw_crime_data.a_crime['DATE_REPORTED'][i],
+                        'DATE_SUBMITTED': timestamp,
+                        'TIME_DELTA': time_delta})
+        
+            process_delta_for_graph(time_delta)
+    #print totals generated from new time data
+        print(">30:", num_above_30)
+        print("<30:", num_30_and_below)
+
+        draw_graph()  
+print('Thanks for your inquiry.')
