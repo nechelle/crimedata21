839769741a52a021929e9db88294cfe0dd143d42
Nechelle Burns <nburns1@gmail.com>
2022-03-29 20:42:59 -0400

    updated menu

--- raw_crime_data.py
+++ raw_crime_data.py
@@ -11,15 +11,19 @@ crime_type = ""
 #create menu for user to choose crime, populates all crimes committed in 2021
 #asks for user input, outputs input
 def crime_menu():
+    print("*******************************************************")
+    print("********** 2021 LMPD Crime Submissions Menu ***********")
+    print("*******************************************************")
+    print("**** Choose a crime, enter 'Q' to quit.            ****")
+    print("*******************************************************")
     crime_list = crime_df.CRIME_TYPE.unique()
     for number, letter in enumerate(crime_list, start=1):
         print(number, letter) 
     
     offense = input("Which criminal offense are you interested in? ")
-    print("You chose to look at the offense: " + offense)
-
+    
     return offense
-#give number corresponding crimt type from specified list
+#give number corresponding crime type from specified list
 def set_crime_search(offense):
   global a_crime
   global crime_type
@@ -55,11 +59,14 @@ def set_crime_search(offense):
     crime_type = 'SEX CRIMES'
   elif offense == '15':
     crime_type = 'DISTURBING THE PEACE'
+  elif offense == 'Q':
+    ret_val = 2
   else:
     ret_val = -1
-    print("Offense not found.")
+    print("Offense not found.\n")
 #create variable for specified crime type retrieved from input
   if ret_val == 0:
+    print("You chose to look at the offense: " + crime_type)
     a_crime = crime_df.loc[crime_df['CRIME_TYPE'] == crime_type]
 
   return ret_val
